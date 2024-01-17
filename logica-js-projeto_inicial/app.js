cd6879ea931cee6a515c8bba2fd0a3a5616e791d
Marcos Schwaab <marcos.schwaab@gmail.com>
2024-01-17 19:51:11 -0300

    add attempts

--- logica-js-projeto_inicial/app.js
+++ logica-js-projeto_inicial/app.js
@@ -1,21 +1,23 @@
 alert ('Welcome the GAME');
 
 let secretNumber = 5;
-let number
-
+let number;
+let attempts = 1;
 
 while (number != secretNumber) {
 
     number = prompt('Choose a number from 1 to 30');
 
     if (secretNumber == number) {
-        console.log(`Congrats, YOU WIN! The number is ${secretNumber}`);
+        console.log(`Congrats, YOU WIN! The number is ${secretNumber} with ${attempts} `);
     } else {
         if ( number > secretNumber){
-            alert(`The number is less than ${number}`)
+            alert(`The number is less than ${number}`);
+
         }
         else {
-            alert(`The number is greater than ${number}`)
+            alert(`The number is greater than ${number}`);
         }
+        attempts++;
     }
 }
