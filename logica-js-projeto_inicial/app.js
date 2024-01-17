59c01cdda775c44857c7e10383687f349b47e43f
Marcos Schwaab <marcos.schwaab@gmail.com>
2024-01-17 19:10:53 -0300

    add else exemple01

--- logica-js-projeto_inicial/app.js
+++ logica-js-projeto_inicial/app.js
@@ -1,2 +1,11 @@
-let mensagemDeBoasVindas = 'Boas vindas ao App';
-alert('mensagemDeBoasVindas');
+alert ('Welcome the GAME');
+
+let secretNumber = 29;
+
+let number = prompt('Choose a number from 1 to 30');
+
+if (secretNumber == number) {
+    console.log('Congrats, YOU WIN! The number is', secretNumber)
+} else {
+    alert('You missed!')
+}
