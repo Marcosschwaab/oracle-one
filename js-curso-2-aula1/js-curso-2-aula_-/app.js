37c4f093dc07f741c40e6acf085e22616d86783b
Marcos Schwaab <marcos.schwaab@gmail.com>
2024-01-22 14:50:10 -0300

    add more functions

--- js-curso-2-aula1/js-curso-2-aula_-/app.js
+++ js-curso-2-aula1/js-curso-2-aula_-/app.js
@@ -1,4 +1,4 @@
-
+let createNumber = createNumberRandom();
 
 function showTextTags(tag, text){
     let element = document.querySelector(tag);
@@ -9,5 +9,9 @@ showTextTags('h1', 'Jogo do Número Secreto');
 showTextTags('p', 'Escolha um número entre 1 a 10');
  
 function checkKick() {
-    console.log('O botão foi clicado');
+    console.log(createNumber);
 }
+
+function createNumberRandom(){
+    return parseInt(Math.random() * 10 + 1);
+} 
