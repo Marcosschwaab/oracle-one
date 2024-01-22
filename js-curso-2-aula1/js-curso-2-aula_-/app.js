0d36a817376f5af8f1b7c1bdbd000510106966c3
Marcos Schwaab <marcos.schwaab@gmail.com>
2024-01-22 14:25:20 -0300

    Add exercise 01 course 02

---
+++ js-curso-2-aula1/js-curso-2-aula_-/app.js
@@ -0,0 +1,9 @@
+let title = document.querySelector('h1');
+title.innerHTML =  'Jogo do Número Secreto';
+
+let paragraf = document.querySelector('p');
+paragraf.innerHTML = 'Escolha um número entre 1 a 10';
+ 
+function checkKick() {
+    console.log('O botão foi clicado');
+}
