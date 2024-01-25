be2e7440520da33baf9bc74eca96fa64ad65c2b8
Marcos Schwaab <marcos.schwaab@gmail.com>
2024-01-25 19:57:56 -0300

    Crie uma lista com 3 nomes e exiba no console apenas o primeiro elemento.

--- desafio03_curso02/app.js
+++ desafio03_curso02/app.js
@@ -3,3 +3,6 @@ let listaGenerica = [];
 let linguagensDeProgramacao = ['JavaScript', 'C', 'C++', 'Kotlin', 'Python'];
 
 linguagensDeProgramacao.push('Java', 'Ruby', 'GoLang');
+
+let nomes = ['Ana', 'Bruno', 'Carlos'];
+console.log(nomes[0]);  // Isto irá exibir 'Ana'
