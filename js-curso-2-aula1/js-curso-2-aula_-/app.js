abaf56c8084dcf73a33aa517625da7eaa95e64ea
Marcos Schwaab <marcos.schwaab@gmail.com>
2024-01-25 19:37:58 -0300

    include list add numbers chosen

--- js-curso-2-aula1/js-curso-2-aula_-/app.js
+++ js-curso-2-aula1/js-curso-2-aula_-/app.js
@@ -1,18 +1,60 @@
-let createNumber = createNumberRandom();
+let listNumbersSort = [];
+
+let secretNumber = createNumberRandom();
+let attemps = 1;
+
 
 function showTextTags(tag, text){
     let element = document.querySelector(tag);
     element.innerHTML = text;
 }
  
-showTextTags('h1', 'Jogo do Número Secreto');
-showTextTags('p', 'Escolha um número entre 1 a 10');
+function showInitialGame(){
+    showTextTags('h1', 'Jogo do Número Secreto');
+    showTextTags('p', 'Escolha um número entre 1 a 10');
+}
  
+showInitialGame();
+
 function checkKick() {
     let kick = document.querySelector('input').value;
-    console.log(kick == createNumber);
+   
+    if (kick == secretNumber){
+        showTextTags('h1', 'Acertou!');
+        let checkAttemps = attemps > 1 ? 'tentativas' : 'tentativa';
+        let msgAttemps = `Você descobriu o número secreto com ${attemps} ${checkAttemps}.`;
+        showTextTags('p', `${msgAttemps}`);
+        document.getElementById('refresh').removeAttribute('disabled');
+    } else {
+        if (kick > secretNumber) {
+            showTextTags('p', 'O número secreto é MENOR, tente outra vez!');
+        } else {
+        showTextTags('p', 'O número secreto é MAIOR, tente outra vez!');
+        }
+        attemps ++;
+        clearKick();
+    }   
 }
 
 function createNumberRandom(){
-    return parseInt(Math.random() * 10 + 1);
+    let numberChosen =  parseInt(Math.random() * 10 + 1);
+        if (listNumbersSort.includes(numberChosen)){
+            return createNumberRandom();
+        }
+        else {
+            listNumbersSort.push(numberChosen);
+            console.log(listNumbersSort);
+            return numberChosen;
+        }
 } 
+function clearKick (){
+    kick = document.querySelector('input');
+    kick.value = '';
+}
+function refreshGame(){
+    secretNumber = createNumberRandom();
+    clearKick();
+    attemps = 1;
+    showInitialGame();
+}
+
